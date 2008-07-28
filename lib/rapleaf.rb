a25db45f25148b02784f5737fbf9e29d11b6c960
Glenn Rempe <glenn@rempe.us>
2008-07-28 16:13:30 -0700

    Removed old comment.

--- lib/rapleaf.rb
+++ lib/rapleaf.rb
@@ -1,9 +1,5 @@
 %w[ base64 cgi openssl digest/sha1 net/http builder ostruct ].each { |f| require f }
 
-# Require any lib files that we have bundled with this Ruby Gem in the lib/EC2 directory.
-# Parts of the EC2 module and Base class are broken out into separate
-# files for maintainability and are organized by the functional groupings defined
-# in the EC2 API developers guide.
 Dir[File.join(File.dirname(__FILE__), 'rapleaf/**/*.rb')].sort.each { |lib| require lib }
 
 module Rapleaf
