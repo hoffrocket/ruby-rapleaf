152c3a895b25c2b97d92fd5aedb17bd74a564306
Glenn Rempe <glenn@rempe.us>
2008-08-27 15:06:53 -0700

    Minor require change

--- lib/rapleaf.rb
+++ lib/rapleaf.rb
@@ -1,6 +1,10 @@
-%w[ base64 cgi openssl digest/sha1 net/http builder ostruct ].each { |f| require f }
+%w[ base64 cgi openssl digest/sha1 net/http builder ostruct ].each do |f|
+  require f
+end
 
-Dir[File.join(File.dirname(__FILE__), 'rapleaf/**/*.rb')].sort.each { |lib| require lib }
+Dir[File.join(File.dirname(__FILE__), 'rapleaf/**/*.rb')].sort.each do |lib|
+  require lib
+end
 
 module Rapleaf
 
